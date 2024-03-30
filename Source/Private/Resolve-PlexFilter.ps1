32e00e913cc23c011ea350c33068e0b95ef5f4a2
Sean Bryla <sean.bryla@gmail.com>
2024-03-30 14:08:48 -0400

    Add resolution for "exact" attributes

--- Source/Private/Resolve-PlexFilter.ps1
+++ Source/Private/Resolve-PlexFilter.ps1
@@ -10,137 +10,169 @@ function Resolve-PlexFilter
         .EXAMPLE
             Resolve-PlexFilter -MatchAny -Filter "DateAdded IsNotInTheLast 2y; Unplayed IsTrue"
         .EXAMPLE
-            Resolve-PlexFilter -MatchAll -Filter "Decade IsLessThan 1990s"
+            Resolve-PlexFilter -MatchAll -Filter "Actor IsNot Robert Pattinson; Title BeginsWith bat" -LibraryID 1
 	#>
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'MatchAny')]
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'MatchAll')]
-
     [CmdletBinding()]
+    [OutputType([string])]
     param(
         [Parameter(Mandatory)]
         [string]
         $Filter,
 
-        [Parameter(ParameterSetName = "All")]
-        [switch]
-        $MatchAll,
+        [Parameter()]
+        [ValidateSet("MatchAll", "MatchAny")]
+        [string]
+        $MatchType = "MatchAll",
 
-        [Parameter(ParameterSetName = "Any")]
+        [Parameter()]
+        [string]
+        $LibraryID,
+
+        [Parameter(DontShow)]
         [switch]
-        $MatchAny
+        $IKnowWhatImDoing
     )
 
-    # Copied from Plex web GUI.
-    # @{Class = @{Operator1, Operator2...}}
-    $Operators = @{
-        String   = @{
-            Contains       = "="
-            DoesNotContain = "!="
-            Is             = "=="
-            IsNot          = "!=="
-            BeginsWith     = "<="
-            EndsWith       = ">="
-        }
-        Numeric  = @{
-            Is            = "="
-            IsNot         = "!="
-            IsGreaterThan = ">>="
-            IsLessThan    = "<<="
-        }
-        Exact    = @{
-            Is    = "="
-            IsNot = "!="
-        }
-        Bool     = @{
-            IsTrue  = "=1"
-            IsFalse = "!=1"
-        }
-        SemiBool = @{
-            Is = "="
-        }
-        Date     = @{
-            IsBefore       = "<<="
-            IsAfter        = ">>="
-            IsInTheLast    = ">>=-"
-            IsNotInTheLast = "<<=-"
-        }
-    }
-
-    # Copied from Plex web GUI and translated into Plex DB attributes.
-    $Attributes = @{
-        Title            = @{ Class = "String" ; Name = "title" }
-        Studio           = @{ Class = "String" ; Name = "studio" }
-        Edition          = @{ Class = "String" ; Name = "editionTitle" }
-        Rating           = @{ Class = "Numeric" ; Name = "userRating" }
-        Year             = @{ Class = "Numeric" ; Name = "year" }
-        Decade           = @{ Class = "Numeric" ; Name = "decade" }
-        Plays            = @{ Class = "Numeric" ; Name = "viewCount" }
-        ContentRating    = @{ Class = "Exact" ; Name = "contentRating" }
-        Genre            = @{ Class = "Exact" ; Name = "genre" }
-        Collection       = @{ Class = "Exact" ; Name = "collection" }
-        Director         = @{ Class = "Exact" ; Name = "director" }
-        Writer           = @{ Class = "Exact" ; Name = "writer" }
-        Producer         = @{ Class = "Exact" ; Name = "producer" }
-        Actor            = @{ Class = "Exact" ; Name = "actor" }
-        Country          = @{ Class = "Exact" ; Name = "country" }
-        SubtitleLanguage = @{ Class = "Exact" ; Name = "subtitleLanguage" }
-        AudioLanguage    = @{ Class = "Exact" ; Name = "audioLanguage" }
-        Label            = @{ Class = "Exact" ; Name = "label" }
-        Unmatched        = @{ Class = "Bool" ; Name = "unmatched" }
-        Duplicate        = @{ Class = "Bool" ; Name = "duplicate" }
-        Unplayed         = @{ Class = "Bool" ; Name = "unwatched" }
-        HDR              = @{ Class = "Bool" ; Name = "hdr" }
-        InProgress       = @{ Class = "Bool" ; Name = "inProgress" }
-        Trash            = @{ Class = "Bool" ; Name = "trash" }
-        Resolution       = @{ Class = "SemiBool" ; Name = "resolution" }
-        ReleaseDate      = @{ Class = "Date" ; Name = "originallyAvailableAt" }
-        DateAdded        = @{ Class = "Date" ; Name = "addedAt" }
-        LastPlayed       = @{ Class = "Date" ; Name = "lastViewedAt" }
-    }
-
-    try
+    end
     {
-        $Query = foreach ($Clause in ($Filter -split ';'))
-        {
-            # Parse out values from clause
-            if (-not ($Clause -match '(?<Attribute>\w+) (?<Operator>\w+)(?: (?<Value>.*))?'))
-            {
-                throw "Unable to parse filter clause '$Clause'. Syntax is '<Attribute> <Operator> <Value>'."
+        # Copied from Plex web GUI.
+        $Operators = @{
+            # Class    = @{
+            #     Operator1 = "..."
+            #     Operator2 = "..."
+            # }
+            String   = @{
+                Contains       = "="
+                DoesNotContain = "!="
+                Is             = "=="
+                IsNot          = "!=="
+                BeginsWith     = "<="
+                EndsWith       = ">="
             }
-
-            # Translate clause into API query and verify that it makes sense.
-            $Attribute = $Attributes[$Matches.Attribute]
-            $Operator = $Operators[$Attribute.Class].($Matches.Operator)
-            if (-not $Attribute)
-            {
-                throw "Unable to parse filter clause '$Clause'. Attribute does not exist in Plex DB."
+            Numeric  = @{
+                Is            = "="
+                IsNot         = "!="
+                IsGreaterThan = ">>="
+                IsLessThan    = "<<="
             }
-            if (-not $Operator)
-            {
-                throw "Unable to parse filter clause '$Clause'. Operator not supported by the attribute."
+            Exact    = @{
+                Is    = "="
+                IsNot = "!="
             }
-
-            #TODO translate Exact class into Plex DB ratingKey
-
-            # Return API query
-            $Attribute.Name, $Operator, $Matches.Value -join ''
+            Bool     = @{
+                IsTrue  = "=1"
+                IsFalse = "!=1"
+            }
+            SemiBool = @{
+                Is = "="
+            }
+            Date     = @{
+                IsBefore       = "<<="
+                IsAfter        = ">>="
+                IsInTheLast    = ">>=-"
+                IsNotInTheLast = "<<=-"
+            }
         }
 
-        # Return entire API query
-        switch ($PSCmdlet.ParameterSetName)
-        {
-            "All" { $Query -join '&' ; continue }
-            "Any" { "push=1&{0}&pop=1" -f ($Query -join '&') ; continue }
-            default { throw "You should not see this." }
+        # Copied from Plex web GUI and translated into Plex DB attributes.
+        $Attributes = @{
+            # NameinGUI        = @{ Class = "ClassName" ; Name = "NameInDB" }
+            Title            = @{ Class = "String" ; Name = "title" }
+            Studio           = @{ Class = "String" ; Name = "studio" }
+            Edition          = @{ Class = "String" ; Name = "editionTitle" }
+            Rating           = @{ Class = "Numeric" ; Name = "userRating" }
+            Year             = @{ Class = "Numeric" ; Name = "year" }
+            Decade           = @{ Class = "Numeric" ; Name = "decade" }
+            Plays            = @{ Class = "Numeric" ; Name = "viewCount" }
+            ContentRating    = @{ Class = "Exact" ; Name = "contentRating" }
+            Genre            = @{ Class = "Exact" ; Name = "genre" }
+            Collection       = @{ Class = "Exact" ; Name = "collection" }
+            Director         = @{ Class = "Exact" ; Name = "director" }
+            Writer           = @{ Class = "Exact" ; Name = "writer" }
+            Producer         = @{ Class = "Exact" ; Name = "producer" }
+            Actor            = @{ Class = "Exact" ; Name = "actor" }
+            Country          = @{ Class = "Exact" ; Name = "country" }
+            SubtitleLanguage = @{ Class = "Exact" ; Name = "subtitleLanguage" }
+            AudioLanguage    = @{ Class = "Exact" ; Name = "audioLanguage" }
+            Label            = @{ Class = "Exact" ; Name = "label" }
+            Unmatched        = @{ Class = "Bool" ; Name = "unmatched" }
+            Duplicate        = @{ Class = "Bool" ; Name = "duplicate" }
+            Unplayed         = @{ Class = "Bool" ; Name = "unwatched" }
+            HDR              = @{ Class = "Bool" ; Name = "hdr" }
+            InProgress       = @{ Class = "Bool" ; Name = "inProgress" }
+            Trash            = @{ Class = "Bool" ; Name = "trash" }
+            Resolution       = @{ Class = "SemiBool" ; Name = "resolution" }
+            ReleaseDate      = @{ Class = "Date" ; Name = "originallyAvailableAt" }
+            DateAdded        = @{ Class = "Date" ; Name = "addedAt" }
+            LastPlayed       = @{ Class = "Date" ; Name = "lastViewedAt" }
         }
-    }
-    catch
-    {
-        if ($_.Exception.Message -match "Index operation failed")
+
+        try
         {
-            throw "Unable to parse filter clause '$Clause'."
+            $Query = foreach ($Clause in ($Filter -split ';'))
+            {
+                # Parse out values from clause
+                if (-not ($Clause -match '(?<Attribute>\w+) (?<Operator>\w+)(?: (?<Value>.*))?'))
+                {
+                    throw "Unable to parse filter clause '$Clause'. Syntax is '<Attribute> <Operator> <Value>'."
+                }
+
+                # Translate clause into API query and verify that it makes sense.
+                $Attribute = $Attributes[$Matches.Attribute]
+                $Operator = $Operators[$Attribute.Class].($Matches.Operator)
+                if (-not $Attribute)
+                {
+                    throw "Unable to parse filter clause '$Clause'. Attribute does not exist in Plex DB."
+                }
+                if (-not $Operator)
+                {
+                    throw "Unable to parse filter clause '$Clause'. Operator not supported by the attribute."
+                }
+
+                # Translate Exact class into Plex DB key
+                if ($Attribute.Class -eq 'Exact')
+                {
+                    if (!$script:PlexConfigData)
+                    {
+                        try
+                        {
+                            Import-PlexConfiguration -WhatIf:$False
+                        }
+                        catch
+                        {
+                            throw $_
+                        }
+                    }
+                    $Uri = Get-PlexAPIUri -RestEndpoint "library/sections/$($PSBoundParameters.LibraryID)/$($Attribute.Name)" -ErrorAction Stop
+                    $Key = ((Invoke-RestMethod -Uri $Uri -Method Get -ErrorAction Stop).MediaContainer.Directory | Where-Object { $_.title -eq $Matches.Value }).key
+                    if (-not ($Key -or $IKnowWhatImDoing))
+                    {
+                        throw ("Unable to find key for '{0}' in library '{1}'." -f $Matches.Value, $PSBoundParameters.LibraryID)
+                    }
+
+                    $Matches.Value = @($Matches.Value, $Key)[[bool]$Key] # Null check for key
+                }
+
+                # Return API query
+                $Attribute.Name, $Operator, $Matches.Value -join ''
+            }
+
+            # Return entire API query
+            switch ($MatchType)
+            {
+                "MatchAll" { $Query -join '&' ; continue }
+                "MatchAny" { "push=1&{0}&pop=1" -f ($Query -join '&or=1&') ; continue }
+                default { throw "You should not see this." }
+            }
         }
-        else { $PSCmdlet.ThrowTerminatingError($_) }
+        catch
+        {
+            if ($_.Exception.Message -match "Index operation failed")
+            {
+                throw "Unable to parse filter clause '$Clause'."
+            }
+            else { $PSCmdlet.ThrowTerminatingError($_) }
+        }
     }
 }
