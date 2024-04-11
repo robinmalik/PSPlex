97ec64423c796cdcaf2b0ecc51b269ccc1c40f69
Robin Malik <8790561+robinmalik@users.noreply.github.com>
2024-04-11 06:22:40 +0100

    UTF8 (remove BOM). Spaces to tabs because everything else is at this point

--- Source/Public/New-PlexSmartCollection.ps1
+++ Source/Public/New-PlexSmartCollection.ps1
@@ -1,123 +1,123 @@
-﻿function New-PlexSmartCollection
+function New-PlexSmartCollection
 {
-    <#
-        .SYNOPSIS
-            Creates a new smart collection.
-        .DESCRIPTION
-            Creates a new smart collection.
-        .PARAMETER Name
-            Name of the smart collection.
-        .PARAMETER LibraryId
-            ID of the library to create the smart collection in.
-        .PARAMETER Filter
-            Specifies the query string that retrieves the items in the smart collection. The syntax matches the Plex Web GUI as closely as possible. Clauses are separated by a semi-colon ( ; ).
+	<#
+		.SYNOPSIS
+			Creates a new smart collection.
+		.DESCRIPTION
+			Creates a new smart collection.
+		.PARAMETER Name
+			Name of the smart collection.
+		.PARAMETER LibraryId
+			ID of the library to create the smart collection in.
+		.PARAMETER Filter
+			Specifies the query string that retrieves the items in the smart collection. The syntax matches the Plex Web GUI as closely as possible. Clauses are separated by a semi-colon ( ; ).
 
-            Syntax:
+			Syntax:
 
-            - <Atttribute> <Operator> <Value>
-            - <Atttribute> <Operator> <Value>;<Atttribute> <Operator> <Value>
+			- <Atttribute> <Operator> <Value>
+			- <Atttribute> <Operator> <Value>;<Atttribute> <Operator> <Value>
 
-            - Attributes:
-                - String
-                    - Title
-                    - Studio
-                    - Edition
-                - Numeric
-                    - Rating
-                    - Year
-                    - Decade
-                    - Plays
-                - Exact
-                    - ContentRating
-                    - Genre
-                    - Collection
-                    - Actor
-                    - Country
-                    - SubtitleLanguage
-                    - AudioLanguage
-                    - Label
-                - Boolean
-                    - Unmatched
-                    - Duplicate
-                    - Unplayed
-                    - HDR
-                    - InProgress
-                    - Trash
-                - Semi-Boolean
-                    - Resolution
-                - Date
-                    - ReleaseDate
-                    - DateAdded
-                    - LastPlayed
+			- Attributes:
+				- String
+					- Title
+					- Studio
+					- Edition
+				- Numeric
+					- Rating
+					- Year
+					- Decade
+					- Plays
+				- Exact
+					- ContentRating
+					- Genre
+					- Collection
+					- Actor
+					- Country
+					- SubtitleLanguage
+					- AudioLanguage
+					- Label
+				- Boolean
+					- Unmatched
+					- Duplicate
+					- Unplayed
+					- HDR
+					- InProgress
+					- Trash
+				- Semi-Boolean
+					- Resolution
+				- Date
+					- ReleaseDate
+					- DateAdded
+					- LastPlayed
 
-                - Operators:
-                - String
-                    - Contains
-                    - DoesNotContain
-                    - Is
-                    - IsNot
-                    - BeginsWith
-                    - EndsWith
-                - Numeric
-                    - Is
-                    - IsNot
-                    - IsGreaterThan
-                    - IsLessThan
-                - Exact
-                    - Is
-                    - IsNot
-                - Boolean
-                    - IsTrue
-                    - IsFalse
-                - Semi-Boolean
-                    - Is
-                - Date
-                    - IsBefore (Value format: yyyy-mm-dd)
-                    - IsAfter (Value format: yyyy-mm-dd)
-                    - IsInTheLast
-                    - IsNotInTheLast
+				- Operators:
+				- String
+					- Contains
+					- DoesNotContain
+					- Is
+					- IsNot
+					- BeginsWith
+					- EndsWith
+				- Numeric
+					- Is
+					- IsNot
+					- IsGreaterThan
+					- IsLessThan
+				- Exact
+					- Is
+					- IsNot
+				- Boolean
+					- IsTrue
+					- IsFalse
+				- Semi-Boolean
+					- Is
+				- Date
+					- IsBefore (Value format: yyyy-mm-dd)
+					- IsAfter (Value format: yyyy-mm-dd)
+					- IsInTheLast
+					- IsNotInTheLast
 
-            - Examples:
-                - "DateAdded IsNotInTheLast 2y; Unplayed IsTrue"
-                - "Title BeginsWith Star Trek; Unplayed IsTrue"
-                - "Actor Is Jim Carrey; Genre Is Comedy"
+			- Examples:
+				- "DateAdded IsNotInTheLast 2y; Unplayed IsTrue"
+				- "Title BeginsWith Star Trek; Unplayed IsTrue"
+				- "Actor Is Jim Carrey; Genre Is Comedy"
 
-        .PARAMETER MatchType
-            Specifies how filter clauses are matched.
+		.PARAMETER MatchType
+			Specifies how filter clauses are matched.
 
-            - MatchAll: Matches all clauses.
-            - MatchAny: Matches any cluase.
-        .EXAMPLE
-            New-PlexSmartCollection -Name "Star Trek" -LibraryId 1 -Filter "Title Contains Star Trek"
-        .EXAMPLE
-            New-PlexSmartCollection -Name "80's" -LibraryId 1 -Filter "Decade Is 1980"
-        .EXAMPLE
-            New-PlexSmartCollection -Name "Old Favorites" -LibraryId 1 -Filter "Plays IsGreaterThan 2; LastPlayed IsNotInTheLast 1y"
-        .EXAMPLE
-            New-PlexSmartCollection -Name "Trek Wars" -LibraryId 1 -MatchType MatchAny -Filter "title contains star trek; title contains star wars"
-    #>
+			- MatchAll: Matches all clauses.
+			- MatchAny: Matches any cluase.
+		.EXAMPLE
+			New-PlexSmartCollection -Name "Star Trek" -LibraryId 1 -Filter "Title Contains Star Trek"
+		.EXAMPLE
+			New-PlexSmartCollection -Name "80's" -LibraryId 1 -Filter "Decade Is 1980"
+		.EXAMPLE
+			New-PlexSmartCollection -Name "Old Favorites" -LibraryId 1 -Filter "Plays IsGreaterThan 2; LastPlayed IsNotInTheLast 1y"
+		.EXAMPLE
+			New-PlexSmartCollection -Name "Trek Wars" -LibraryId 1 -MatchType MatchAny -Filter "title contains star trek; title contains star wars"
+	#>
 
-    [CmdletBinding(SupportsShouldProcess)]
-    param(
-        [Parameter(Mandatory)]
-        [string]
-        $Name,
+	[CmdletBinding(SupportsShouldProcess)]
+	param(
+		[Parameter(Mandatory)]
+		[string]
+		$Name,
 
-        [Parameter(Mandatory)]
-        [string]
-        $LibraryId,
+		[Parameter(Mandatory)]
+		[string]
+		$LibraryId,
 
-        [Parameter(Mandatory)]
-        [string]
-        $Filter,
+		[Parameter(Mandatory)]
+		[string]
+		$Filter,
 
-        [Parameter()]
-        [ValidateSet("MatchAll", "MatchAny")]
-        [string]
-        $MatchType = "MatchAll"
-    )
+		[Parameter()]
+		[ValidateSet("MatchAll", "MatchAny")]
+		[string]
+		$MatchType = "MatchAll"
+	)
 
-    #############################################################################
+	#############################################################################
 	#Region Import Plex Configuration
 	if(!$script:PlexConfigData)
 	{
@@ -132,20 +132,23 @@
 	}
 	#EndRegion
 
-    #############################################################################
+	#############################################################################
 	#Region Check if collection already exists
-    try {
-        $Collections = Get-PlexCollection -LibraryId $LibraryId
-        if ($Collections.title -contains $Name){
-            throw "Collection '$Name' already exits"
-        }
-    }
-    catch {
-        $PSCmdlet.ThrowTerminatingError($_)
-    }
-    #EndRegion
+	try
+	{
+		$Collections = Get-PlexCollection -LibraryId $LibraryId
+		if ($Collections.title -contains $Name)
+		{
+			throw "Collection '$Name' already exits"
+		}
+	}
+	catch
+	{
+		$PSCmdlet.ThrowTerminatingError($_)
+	}
+	#EndRegion
 
-    #############################################################################
+	#############################################################################
 	#Region Get machine identifier
 	Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Getting list of Plex servers (to get machine identifier)"
 	try
@@ -162,28 +165,30 @@
 	}
 	#EndRegion
 
-    #############################################################################
+	#############################################################################
 	#Region Construct Uri
-    try {
-        $Items = Resolve-PlexFilter -MatchType $MatchType -LibraryId $LibraryId -Filter $Filter
-        $Params = [ordered]@{
-            type = '1'
-            title = [System.Uri]::EscapeDataString($Name)
-            smart = '1'
-            sectionId = $LibraryId
-            uri = [System.Uri]::EscapeDataString("server://$($CurrentPlexServer.machineIdentifier)/com.plexapp.plugins.library/library/sections/$LibraryId/all?type=1&sort=titleSort&$Items")
-        }
+	try
+	{
+		$Items = Resolve-PlexFilter -MatchType $MatchType -LibraryId $LibraryId -Filter $Filter
+		$Params = [ordered]@{
+			type      = '1'
+			title     = [System.Uri]::EscapeDataString($Name)
+			smart     = '1'
+			sectionId = $LibraryId
+			uri       = [System.Uri]::EscapeDataString("server://$($CurrentPlexServer.machineIdentifier)/com.plexapp.plugins.library/library/sections/$LibraryId/all?type=1&sort=titleSort&$Items")
+		}
 
-        $DataUri = (Get-PlexAPIUri -RestEndpoint "library/collections" -Params $Params)
-    }
-    catch {
-        $PSCmdlet.ThrowTerminatingError($_)
-    }
-    #EndRegion
+		$DataUri = (Get-PlexAPIUri -RestEndpoint "library/collections" -Params $Params)
+	}
+	catch
+	{
+		$PSCmdlet.ThrowTerminatingError($_)
+	}
+	#EndRegion
 
-    #############################################################################
+	#############################################################################
 	#Region Make request
-    if($PSCmdlet.ShouldProcess("Library $LibraryId", "Create Smart Collection '$Name'"))
+	if($PSCmdlet.ShouldProcess("Library $LibraryId", "Create Smart Collection '$Name'"))
 	{
 		Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Creating Smart Collection $Name in Libary '$LibraryId'"
 		try
